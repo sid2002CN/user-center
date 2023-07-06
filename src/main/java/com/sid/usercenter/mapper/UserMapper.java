3add7c422f70112c6b09abef06ea7c3bfa6b27ee
sid <sid@10-19-216-70.dynapool.wireless.nyu.edu>
2023-07-06 14:29:54 -0400

    basic

---
+++ src/main/java/com/sid/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.sid.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sid.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/test/java/com/sid/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
